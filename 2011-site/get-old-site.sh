d5339efb82fb4e8ad3d3299701b1b04f4cc9ccda
Matthew James Wright <matt@ccrma.stanford.edu>
2021-02-01 17:49:42 -0800

    further thoughts

--- 2011-site/get-old-site.sh
+++ 2011-site/get-old-site.sh
@@ -11,3 +11,9 @@ exit
 # Add a .html suffix to every filename (that isn't a directory)
 
 for f in `find web.archive.org  -type f`; do mv $f $f.html ; done
+
+
+# Keep only the latest version of each file
+
+# Remove the boring junk (navigation, etc.) from each file
+

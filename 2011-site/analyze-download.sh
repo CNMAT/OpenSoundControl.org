889c38ccb28812d30a5070b4b06979ecce91b019
Matthew James Wright <matt@ccrma.stanford.edu>
2021-02-05 10:40:43 -0800

    further musings

--- 2011-site/analyze-download.sh
+++ 2011-site/analyze-download.sh
@@ -23,3 +23,14 @@ for f in `find web.archive.org/ -name 7`; do echo $f; ls -l $f; done
 # These have differences only in the archive.org part, not the content part:
 diff web.archive.org//web/20201020081252/http:/opensoundcontrol.org/forum/7 web.archive.org//web/20201025222223/http:/opensoundcontrol.org/forum/7
 
+
+
+# Having downloaded different versions of the site (with different
+# wget depths) and cleaned them up, do they differ other than in the
+# link timestamps?
+
+diff -r clean3 clean4 | grep -v //web.archive.org/web/ | grep -v /http://opensoundcontrol.org/
+
+diff -r  clean5/ clean6 | grep -v //web.archive.org/web/ | grep -v /http://opensoundcontrol.org/ | more
+
+# clean6 has more under /topic - could consider a separate crawl focused there?

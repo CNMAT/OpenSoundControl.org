96d02a2102b0bd5b6ed215057390f7bb92abeca5
Matthew James Wright <matt@ccrma.stanford.edu>
2021-02-08 17:21:22 -0800

    the script that created the md files in the previous commit

---
+++ legacy/2011-site/convert-all-to-md.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# convert-all-to-md.sh
+# find all html files in a directory and convert each to .md
+
+if test $# -ne 1; then
+    echo usage: $0 [dir]
+    exit -1
+fi
+dir=$1
+
+find $dir -name .DS_Store -delete
+
+for f in `find $dir -type f -name \*.html ! -size 0`; do
+    # echo f is $f
+    fmd=${f%????}md
+    # echo converting $f to $fmd
+
+    if [ -f $fmd ]; then
+        echo $fmd already exists
+        exit -1;
+    fi
+
+    echo pandoc $f -o $fmd
+    pandoc $f -o $fmd
+
+done
+

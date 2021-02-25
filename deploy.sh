e1fd1c355ead4ec5f59c4214ec72258f6156e37d
Matthew James Wright <matt@ccrma.stanford.edu>
2021-02-25 13:28:31 -0800

    rsync -L so local soft links turn into regular files on the web server

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
-rsync -avzh --delete \
+rsync -aLvzh --delete \
       --exclude .git --exclude \*~ --exclude raw --exclude .DS_Store \
       --exclude legacy \
       --exclude navigation-menu.md \
       --exclude "Implementations (Responses) - Form Responses 1.tsv" \
       --exclude implementations-google-column-names.tsv \
       . ccrma-gate.stanford.edu:/user/m/matt/Library/Web/OSC
+
+# rsync -avzh --delete legacy/2011-site/clean/implementation/implementations-legacy.tsv \
+#      ccrma-gate.stanford.edu:/user/m/matt/Library/Web/OSC
 
 open https://ccrma.stanford.edu/~matt/OSC

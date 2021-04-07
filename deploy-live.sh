8b1249a8edcf7165b49a170f0c7f36a010d4ca2b
Matthew James Wright <matt@ccrma.stanford.edu>
2021-04-07 10:30:52 -0700

    Don't deploy new navigation-menu0 / 1 files

--- deploy-live.sh
+++ deploy-live.sh
@@ -3,7 +3,7 @@ echo Deploying from this computer to the live site\!
 rsync -aLvzh --delete \
       --exclude .git --exclude \*~ --exclude raw --exclude .DS_Store \
       --exclude legacy \
-      --exclude navigation-menu.md \
+      --exclude navigation-menu\*.md \
       --exclude "Implementations (Responses) - Form Responses 1.tsv" \
       --exclude implementations-google-column-names.tsv \
       . ccrma-gate.stanford.edu:/usr/ccrma/web/html/groups/osc
